9ea608656ea3eec2de0de93881144ce94269f1e4
Karim Yassine <karimwebdevelopment@gmail.com>
2021-07-16 12:13:59 -0300

    corrected veryLightGrayishCyan color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
         darkGrayishCyan: "hsl(186, 14%, 43%)",
         grayishCyan: "hsl(184, 14%, 56%)",
         lightGrayishCyan: "hsl(185, 41%, 84%)",
-        veryLightGrayishCyan: "hsl(189, 41%, 97%)",
+        veryLightGrayishCyan: "hsl(203, 50%, 97%)",
         white: "hsl(0, 0%, 100%)",
       },
     },
