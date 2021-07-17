8cae4c64006cee16b704360752e1bdb0f3c04d8d
Karim Yassine <karimwebdevelopment@gmail.com>
2021-07-17 00:32:47 -0300

    added form event listener

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,1 +1,21 @@
 import "../styles/index.css";
+import Splitter from "./handleCalc";
+
+const form = document.forms.spliterForm;
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const bill = form.bill.value;
+  const tipAmount = 5;
+  const people = form.people.value;
+  const calc = new Splitter(bill, tipAmount, people);
+
+  try {
+    calc.handleValidation();
+    Splitter.displayTip(Splitter.formatValue(calc.handleTip()));
+    Splitter.displayTotal(Splitter.formatValue(calc.handleTotal()));
+  } catch (error) {
+    console.log(error);
+  }
+});
