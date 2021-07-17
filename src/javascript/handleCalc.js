b8fd8a32fa67b49c61d364d2aeff09d713a2a313
Karim Yassine <karimwebdevelopment@gmail.com>
2021-07-17 15:23:05 -0300

    git add validation and code refactor

--- src/javascript/handleCalc.js
+++ src/javascript/handleCalc.js
@@ -3,18 +3,38 @@ import Utils from "./utils";
 export default class Splitter extends Utils {
   constructor(bill, tip, people) {
     super();
-    this.bill = bill;
-    this.tipAmount = tip;
-    this.people = people;
+    this.bill = Number(bill);
+    this.tipAmount = Number(tip);
+    this.people = Number(people);
   }
 
   handleValidation() {
-    if (this.bill.trim() === "") {
-      throw new Error("cannot be empty");
+    const billElement = Utils.getQuery("#bill-error-log");
+    const billParent = billElement.parentElement.nextElementSibling;
+    const peopleElement = Utils.getQuery("#people-error-log");
+    const peopleParent = peopleElement.parentElement.nextElementSibling;
+    let error = false;
+
+    if (this.bill <= 0) {
+      billParent.classList.add("border-red");
+      billElement.textContent = "can't be 0 or less";
+      error = true;
+    } else {
+      billParent.classList.remove("border-red");
+      billElement.textContent = null;
     }
 
-    if (this.people.trim() === "") {
-      throw new Error("cannot be empty");
+    if (this.people <= 0) {
+      peopleParent.classList.add("border-red");
+      peopleElement.textContent = "can't be 0 or less";
+      error = true;
+    } else {
+      peopleParent.classList.remove("border-red");
+      peopleElement.textContent = null;
+    }
+
+    if (error === true) {
+      throw new Error("can't be 0 or less");
     }
   }
 
@@ -25,24 +45,24 @@ export default class Splitter extends Utils {
   }
 
   handleTotal() {
-    return Splitter.formatValue(this.handleTip() * this.people);
+    return Splitter.formatValue(this.handleTip() * this.people + this.bill);
   }
 
   static formatValue(value) {
     return value.toLocaleString("en-US", {
-      minimunFractionDigits: 3,
+      minimunFractionDigits: 2,
       maximumFractionDigits: 2,
     });
   }
 
   static displayTip(tip) {
-    const element = document.getElementById("tip-amount-displayer");
+    const element = Utils.getQuery("#tip-amount-displayer");
 
     element.innerText = `$${tip}`;
   }
 
   static displayTotal(total) {
-    const element = document.getElementById("total-amount-displayer");
+    const element = Utils.getQuery("#total-amount-displayer");
 
     element.innerText = `$${total}`;
   }
