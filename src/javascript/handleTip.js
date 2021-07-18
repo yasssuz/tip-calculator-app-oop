dc722fd41df899b0ea7022527ea5d4962a589bec
Karim Yassine <karimwebdevelopment@gmail.com>
2021-07-18 10:55:58 -0300

    added validation and get tip amount functions

--- src/javascript/handleTip.js
+++ src/javascript/handleTip.js
@@ -1,6 +1,22 @@
 import Utils from "./utils";
 
 export default class TipSelector extends Utils {
+  constructor(tip) {
+    super();
+    this.tip = tip;
+  }
+
+  validateTip() {
+    const errLog = Utils.getQuery("#tip-error-log");
+
+    if (Number.isNaN(this.tip)) {
+      errLog.textContent = "select a tip";
+      throw new Error("select a tip first");
+    }
+
+    errLog.textContent = "";
+  }
+
   static clearTips() {
     const tips = Utils.getAllQueries("#tips-list li");
 
@@ -12,5 +28,19 @@ export default class TipSelector extends Utils {
 
     TipSelector.clearTips();
     tip.classList.add("selected-tip");
+  }
+
+  static getTipAmount() {
+    const tips = Utils.getAllQueries("#tips-list li");
+    let tipAmount;
+
+    tips.forEach(tip => {
+      if (tip.classList.contains("selected-tip")) {
+        tipAmount = tip.children[0].textContent;
+        tipAmount = tipAmount.replace(/%/, "");
+      }
+    });
+
+    return Number(tipAmount);
   }
 }
